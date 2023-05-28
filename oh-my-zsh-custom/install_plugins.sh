62acbe9f0ededd29de2594b60e20043bd90df15e
Vladimirs Kotovs <vladimir@kotov.lv>
2023-05-28 23:06:22 +0200

    Install custom zsh plugins

--- oh-my-zsh-custom/install_plugins.sh
+++ oh-my-zsh-custom/install_plugins.sh
@@ -1,6 +1,6 @@
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.dotfiles/oh-my-zsh-custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.dotfiles/oh-my-zsh-custom/plugins/zsh-autosuggestions
 
 # Add to .zshrc
 # plugins=(
